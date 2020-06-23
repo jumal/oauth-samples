cc4a495b89d7486bad8341b5de69cb5c8775cd7b
Jean Philippe Courson <jean-philippe.courson@callsign.com>
2020-06-23 13:13:59 +0100

    Initial commit

--- login/src/main/java/com/sample/oauth/login/LoginApplication.java
+++ login/src/main/java/com/sample/oauth/login/LoginApplication.java
@@ -2,46 +2,11 @@ package com.sample.oauth.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
-import static org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest.toAnyEndpoint;
 
 @SpringBootApplication
-@EnableReactiveMethodSecurity
 public class LoginApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LoginApplication.class, args);
-    }
-
-    @Bean
-    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        return http
-                .authorizeExchange(authorizeExchange -> authorizeExchange
-                        .pathMatchers("/error", "/webjars/**").permitAll()
-                        .matchers(toAnyEndpoint()).permitAll()
-                        .anyExchange().authenticated()
-                )
-                .oauth2Login()
-                .and().csrf().disable()
-                .cors()
-                .and().build();
-    }
-
-    @Bean
-    @ConditionalOnProperty(name = "corsEnabled")
-    CorsConfigurationSource corsConfigurationSource() {
-        UrlBasedCorsConfigurationSource configurationSource = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.applyPermitDefaultValues();
-        configurationSource.registerCorsConfiguration("/**", configuration);
-        return configurationSource;
     }
 }
---
+++ login/src/main/java/com/sample/oauth/login/configuration/SecurityConfiguration.java
@@ -0,0 +1,42 @@
+package com.sample.oauth.login.configuration;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import static org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest.toAnyEndpoint;
+
+@Configuration
+@EnableReactiveMethodSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(authorizeExchange -> authorizeExchange
+                        .pathMatchers("/error", "/webjars/**").permitAll()
+                        .matchers(toAnyEndpoint()).permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2Login()
+                .and().csrf().disable()
+                .cors()
+                .and().build();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "corsEnabled")
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource configurationSource = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.applyPermitDefaultValues();
+        configurationSource.registerCorsConfiguration("/**", configuration);
+        return configurationSource;
+    }
+}
